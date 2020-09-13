b8f63ae0fca20c49f12cc0308e426c28ae7f3aeb
broganmurphy1 <57953502+broganmurphy1@users.noreply.github.com>
2020-09-13 21:19:33 +0100

    Add hashing - level 3

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
-
-
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -21,7 +19,6 @@ const userSchema = new mongoose.Schema({
   password: String
 })
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -57,7 +54,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser) {
     if(err)
